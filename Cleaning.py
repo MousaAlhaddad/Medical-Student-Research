b949e60adc465ddf94cfbbab8a7f4cd18cf4d7b4
Mousa Alhaddad <mousa.j.alhaddad@gmail.com>
2019-05-05 21:50:02 +0300

    Update Cleaning.py

--- Cleaning.py
+++ Cleaning.py
@@ -25,3 +25,21 @@ df["Five Year Impact Factor"] = [x if x != "Not Available" else np.NAN for x in 
 
 # Rename the the columns' names to enable method chaining
 df.columns=[x.replace(" ", "_") for x in list(df.columns)]
+
+# Create a new boolean column for cited articles 
+df["Cited"] = df.Citation_Counts > 0
+
+# Convert the Journal_Impact_Factor continuous variable into a new categorical variable
+Min, Max = df.Journal_Impact_Factor.min(), df.Journal_Impact_Factor.max()
+df["Impact_Factor_Categories"]=df.Journal_Impact_Factor.apply(cut, CutPoints = [Min,1,2,4,6,Max])
+
+# Convert the Citation_Counts continuous variable into a new categorical variable
+Min, Max = df.Citation_Counts.min(), df.Citation_Counts.max()
+df["Citation_Categories"]=df.Citation_Counts.apply(cut, CutPoints = [Min,Min,2,5,10,20,Max])
+df["Citation_Categories"]=[x if x != ">0.0 & <=0.0" else "=0" for x in df['Citation_Categories']]
+
+# Change the Solely_by_students data type from string to boolean
+df.Solely_by_students = df.Solely_by_students.map({"No":False,"Yes":True})
+
+# Change the Is_1st_Author_a_Student data type from string to boolean
+df.Is_1st_Author_a_Student = df.Is_1st_Author_a_Student.map({"No":False,"Yes":True})
